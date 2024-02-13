d85fae7fca24239c4cf43f5642a560add59a67b9
Vadim13245 <159133092+Vadim13245@users.noreply.github.com>
2024-02-14 01:05:14 +0300

    234

--- js/task-3.js
+++ js/task-3.js
@@ -1,13 +1,16 @@
 function checkForSpam(message) {
     let result;
     message = message.toLowerCase()
+
     if(message.includes ("spam") || message.includes ("sale")) {
         return (true)
     } else {
         return (false)
     }
+
     return result;
 }
+
 console.log(checkForSpam("Latest technology news")); // false
 console.log(checkForSpam("JavaScript weekly newsletter")); // false
 console.log(checkForSpam("Get best sale offers now!")); // true
